d77864945e2846790af038cdea58168257dc83a2
Ralph Plumley <ralphplumley@gmail.com>
2018-03-07 19:57:59 -0500

    complete 23: countValuesInObj

--- src/recursion.js
+++ src/recursion.js
@@ -496,6 +496,19 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+	var count = 0;
+
+	for (key in obj) {
+		if (obj[key] === value) {
+			count++;
+		}
+
+		if (typeof obj[key] === 'object') {
+			count += countValuesInObj(obj[key], value);
+		}
+	}
+
+	return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
